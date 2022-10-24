4fedf8a86a50640e8924e123e62eb3d10988d159
M.Ghanem <86367386+o0bscure@users.noreply.github.com>
2022-10-24 19:59:05 +0300

    Update shape_calculator.py

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,4 +1,7 @@
+
+
 class Rectangle:
+    sides = 0
     object_list= []
     def __init__(self,width=int(),height=int()):
         self.width = width
@@ -7,9 +10,11 @@ class Rectangle:
     
     def set_width(self,width=int()):
         self.width = width
+        self.sides = width
     
     def set_height(self,height=int()):
         self.height = height
+        self.sides = height
     
     def get_area(self):
         return self.width * self.height
@@ -21,18 +26,30 @@ class Rectangle:
         return (self.width ** 2 + self.height ** 2) ** .5
     
     def get_picture(self):
-        marker = self.width * "*"
-        lines = self.height
-        result = ""
-        for line in range(lines):
-            result = result + marker + "\n"
-        return result
-    
+        if self.width > 50 or self.height > 50:
+            return "Too big for picture."
+        else:   
+            marker = self.width * "*"
+            lines = self.height
+            result = ""
+            for line in range(lines):
+                result = result + marker + "\n"
+            return result
+        
     def get_amount_inside(self,object):
         if not object in Rectangle.object_list:
             return f"{object} is not an object"
         else:
-            return self.get_area()/object.sides
+            if not object is self:
+                return self.get_area()/object.sides  
+            else:
+                if object == self:
+                    if self.height == object.height and self.width == object.width:
+                        return 1
+                    else:
+                        return 0 
+            #return self.get_area()/(self.get_perimeter()/2)
+            
     
     def __repr__(self):
         return f"{self.__class__.__name__}(width={self.width}, height={self.height})"
@@ -42,6 +59,7 @@ class Square(Rectangle):
     def __init__(self, side_length):
         self.width = side_length
         self.height = side_length
+        self.sides = side_length
         Rectangle.object_list.append(self)
     
     def set_side(self,sides=str()):
@@ -50,24 +68,7 @@ class Square(Rectangle):
         self.height = sides    
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(sides={self.sides})"
+        return f"{self.__class__.__name__}(side={self.sides})"
 
-
-rect = Rectangle(10, 5)
-print(rect.get_area())
-rect.set_height(3)
-print(rect.get_perimeter())
-print(rect)
-print(rect.get_picture())
-
-sq = Square(9)
-print(sq.get_area())
-sq.set_side(4)
-print(sq.get_diagonal())
-print(sq)
-print(sq.get_picture())
-
-rect.set_height(8)
-rect.set_width(16)
-print(Rectangle.object_list)
+rect = Rectangle(15,10)
 print(rect.get_amount_inside(sq))
